87ec0debe6d5d60a4742224f4b08c8aea9eac120
Ramon Lopez <ramon@fastmicroservices.com>
2018-06-26 09:20:34 +0700

    First push

---
+++ whatever/src/main/java/com/ramondev/learn/whatever/WhateverApplication.java
@@ -0,0 +1,12 @@
+package com.ramondev.learn.whatever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhateverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhateverApplication.class, args);
+	}
+}
